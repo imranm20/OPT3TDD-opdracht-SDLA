2bef13dbc28a6a51adc1ae4eed4a0295855a8297
imranm20 <80264994+imranm20@users.noreply.github.com>
2021-05-09 22:48:13 +0200

    Eqtest en afronding van al mijn features

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
 
         SDLAMain main = new SDLAMain();
@@ -16,142 +14,136 @@ public class Main {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        System.out.println(main.opslag.size());
     }
 }
-  class SDLAMain {
-        Scanner s = new Scanner(System.in);
 
-        public final ArrayList<deadline> opslag = new ArrayList<deadline>();
+class SDLAMain {
+    Scanner s = new Scanner(System.in);
 
-        public void addDeadline() throws ParseException {
-            System.out.println("Welkom bij het toevoegen van een deadline, als u wilt stoppen  type dan" + "'stop'");
-            System.out.println("Geef de datum in dit format: jjjj-mm-dd");
+    public final ArrayList<deadline> opslag = new ArrayList<deadline>();
 
-            String regDate = s.nextLine();
-            LocalDate vandaag = LocalDate.now();
-      LocalDate date1 = LocalDate.parse(regDate);
-            int comparison= date1.compareTo(vandaag);
-            if (regDate.equals("stop") || (comparison > -1) ) {
+    public void addDeadline() throws ParseException {
+        System.out.println("Welkom bij het toevoegen van een deadline, als u wilt stoppen  type dan" + "'stop'");
+        System.out.println("Geef de datum in dit format: jjjj-mm-dd");
+
+        String regDate = s.nextLine();
+        LocalDate vandaag = LocalDate.now();
+        LocalDate date1 = LocalDate.parse(regDate);
+        int comparison = date1.compareTo(vandaag);
+        if (regDate.equals("stop") || (comparison > -1)) {
+            menu();
+        }
+
+        System.out.println("Geef de naam van de deadline:");
+        String naam1 = s.nextLine();
+        if (naam1.equals("stop")) {
+            menu();
+        }
+        s.nextLine();
+        System.out.println("Geef de naam van het vak:");
+        String vaknaam1 = s.nextLine();
+        if (vaknaam1.equals("stop")) {
+            menu();
+        }
+        s.nextLine();
+        System.out.println("Geef de informatie over deze deadline:");
+        String info1 = s.nextLine();
+        if (info1.equals("stop") || info1.length() <= 249) {
+            menu();
+        }
+        s.nextLine();
+        System.out.println("Voer de soort deadline in ( toets/huiswerk )");
+        String dd = s.nextLine();
+        if (dd.equals("stop") || !(dd.equalsIgnoreCase("toets") || dd.equalsIgnoreCase("huiswerk"))) {
+            menu();
+        }
+        if (dd.equalsIgnoreCase("toets")) {
+            s.nextLine();
+            System.out.println("Voer het aantal studiepunten in(geheel nummer))");
+            String stdinvoer = s.nextLine();
+            if (stdinvoer.equals("stop")) {
                 menu();
             }
+            int std = Integer.parseInt(stdinvoer);
+            s.nextLine();
+            System.out.println("Voer het aantal minuten in(geheel nummer))");
+            String mininvoer = s.nextLine();
+            if (mininvoer.equals("stop")) {
+                menu();
+            }
+            int duur = Integer.parseInt(mininvoer);
+            s.nextLine();
+            System.out.println("Geef het lokaalnummer");
+            String lokaal = s.nextLine();
+            if (lokaal.equals("stop")) {
+                menu();
+            } else {
+                toets t1 = new toets(date1, naam1, vaknaam1, info1, std, duur, lokaal);
+                opslag.add(t1);
+            }
 
-            System.out.println("Geef de naam van de deadline:");
-            String naam1 = s.nextLine();
-            if (naam1.equals("stop") ) {
+        } else if (dd.equalsIgnoreCase("huiswerk")) {
+            System.out.println("Voer je teamgenoten in");
+            String team1 = s.nextLine();
+            if (team1.equals("stop")) {
                 menu();
             }
             s.nextLine();
-            System.out.println("Geef de naam van het vak:");
-            String vaknaam1 = s.nextLine();
-            if (vaknaam1.equals("stop")) {
+            System.out.println("Geef de prioriteit op een schaal van tien(geheel nummer)");
+            String prio = s.nextLine();
+            int prioriteit = Integer.parseInt(prio);
+            if (prio.equals("stop")) {
                 menu();
+            } else {
+                huiswerk h1 = new huiswerk(date1, naam1, vaknaam1, info1, team1, prioriteit);
+                opslag.add(h1);
             }
-            s.nextLine();
-            System.out.println("Geef de informatie over deze deadline:");
-            String info1 = s.nextLine();
-            if (info1.equals("stop") || info1.length() <= 249) {
-                menu();
-            }
-            s.nextLine();
-            System.out.println("Voer de soort deadline in ( toets/huiswerk )");
-            String dd = s.nextLine();
-            if (dd.equals("stop") || !(dd.equalsIgnoreCase("toets") || dd.equalsIgnoreCase("huiswerk"))) {
-                menu();
-            }
-            if (dd.equalsIgnoreCase("toets")) {
-                s.nextLine();
-                System.out.println("Voer het aantal studiepunten in(geheel nummer))");
-                String stdinvoer = s.nextLine();
-                if (stdinvoer.equals("stop")) {
-                    menu();
-                }
-                int std = Integer.parseInt(stdinvoer);
-                s.nextLine();
-                System.out.println("Voer het aantal minuten in(geheel nummer))");
-                String mininvoer = s.nextLine();
-                if (mininvoer.equals("stop")) {
-                    menu();
-                }
-                int duur = Integer.parseInt(mininvoer);
-                s.nextLine();
-                System.out.println("Geef het lokaalnummer");
-                String lokaal = s.nextLine();
-                if (lokaal.equals("stop")) {
-                    menu();
-                } else {
-                    toets t1 = new toets(date1, naam1, vaknaam1, info1, std, duur, lokaal);
-                    opslag.add(t1);
-                }
-
-            }
-
-            else if (dd.equalsIgnoreCase("huiswerk")) {
-                System.out.println("Voer je teamgenoten in");
-                String team1 = s.nextLine();
-                if (team1.equals("stop")) {
-                    menu();
-                }
-                s.nextLine();
-                System.out.println("Geef de prioriteit op een schaal van tien(geheel nummer)");
-                String prio = s.nextLine();
-                int prioriteit = Integer.parseInt(prio);
-                if (prio.equals("stop")) {
-                    menu();
-                } else {
-                    huiswerk h1 = new huiswerk(date1, naam1, vaknaam1, info1, team1, prioriteit);
-                    opslag.add(h1);
-                }
-
-
-            }
-
 
         }
-
-        public void menu() {
-            System.out.println("welkom bij het menu");
-            System.exit(0);
-        }
-    }
-
-
-    abstract class deadline {
-        LocalDate datum;
-        String naam = "";
-        String vak = "";
-        String informatie = "";
-
-        public deadline(LocalDate d, String x, String y, String z) {
-            this.datum = d;
-            this.naam = x;
-            this.vak = y;
-            this.informatie = z;
-        }
-
 
     }
 
-    class toets extends deadline {
-        int studiepunten;
-        int Minutenduur;
-        String lokaal;
+    public void menu() {
+        System.out.println("welkom bij het menu");
+        System.exit(0);
+    }
+}
 
-        public toets(LocalDate d, String x, String y, String z, int studiepunten, int minutenduur, String lokaal) {
-            super(d, x, y, z);
-            this.studiepunten = studiepunten;
-            this.Minutenduur = minutenduur;
-            this.lokaal = lokaal;
-        }
+abstract class deadline {
+    LocalDate datum;
+    String naam;
+    String vak;
+    String informatie;
+
+    public deadline(LocalDate d, String x, String y, String z) {
+        this.datum = d;
+        this.naam = x;
+        this.vak = y;
+        this.informatie = z;
     }
 
-    class huiswerk extends deadline {
-        String teamgenoten = "";
-        int prioriteitOpSchaalVanTien;
+}
 
-        public huiswerk(LocalDate d, String x, String y, String z, String teamgenoten, int prioriteitOpSchaalVanTien) {
-            super(d, x, y, z);
-            this.teamgenoten = teamgenoten;
-            this.prioriteitOpSchaalVanTien = prioriteitOpSchaalVanTien;
-        }
+class toets extends deadline {
+    int studiepunten;
+    int Minutenduur;
+    String lokaal;
+
+    public toets(LocalDate d, String x, String y, String z, int studiepunten, int minutenduur, String lokaal) {
+        super(d, x, y, z);
+        this.studiepunten = studiepunten;
+        this.Minutenduur = minutenduur;
+        this.lokaal = lokaal;
     }
+}
+
+class huiswerk extends deadline {
+    String teamgenoten;
+    int prioriteitOpSchaalVanTien;
+
+    public huiswerk(LocalDate d, String x, String y, String z, String teamgenoten, int prioriteitOpSchaalVanTien) {
+        super(d, x, y, z);
+        this.teamgenoten = teamgenoten;
+        this.prioriteitOpSchaalVanTien = prioriteitOpSchaalVanTien;
+    }
+}
--- src/SDLAMainTest.java
+++ src/SDLAMainTest.java
@@ -1,16 +1,24 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.text.ParseException;
 import java.time.LocalDate;
-import java.util.Date;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SDLAMainTest{
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SDLAMainTest {
+    public boolean eqMethode(String x, LocalDate y) {
+        LocalDate vandaag = LocalDate.now();
+        int comparison = y.compareTo(vandaag);
+        //zelfde voorwaarde als in de addDeadline methode
+        return x.length() <= 249 && comparison > -1;
+    }
+
+    @Order(2)
     @Test
-    void equivalntietest(){
-        String tekst1  = "Dit\t\t zijn\t\t200\t\tkarakters\t\n" +
+    void equivalntietest() {
+        String tekst1 = "Dit\t\t zijn\t\t200\t\tkarakters\t\n" +
                 "Dit\t\t zijn\t\t200\t\tkarake\t\n" +
                 "Dit\t\t zijn\t\t200\t\tkarakters\t\n" +
                 "Dit\t\t zijn\t\t200\t\tkarakters\t\n" +
@@ -19,17 +27,17 @@ class SDLAMainTest{
                 "Dit\t\t zijn\t\t200\t\tkarakters\t\n" +
                 "Dit\t\t zijn\t\t200\t\tkarakters\t\n" +
                 "Dit\t\t zijn\t\t200kar\t\t\t\t      \n";
-        String tekst2  = "";
-        String tekst3  = "Dit zijn 100  karakters..\n" +
+        String tekst2 = "";
+        String tekst3 = "Dit zijn 100  karakters..\n" +
                 "Dit zijn 100  karakters..\n" +
                 "\n" +
                 "Dit zijn 100  karakters.....\n" +
                 "\n" +
                 "Dit zijn 100  ..\n";
 
-        LocalDate datum1 = LocalDate.of(1999,1,1);
-        LocalDate datum2 =  LocalDate.now();
-        LocalDate datum3 =   LocalDate.of(2022,11,11);
+        LocalDate datum1 = LocalDate.of(1999, 1, 1);
+        LocalDate datum2 = LocalDate.now();
+        LocalDate datum3 = LocalDate.of(2022, 11, 11);
 
 
         assertFalse(eqMethode(tekst3, datum1));
@@ -40,61 +48,91 @@ class SDLAMainTest{
         assertTrue(eqMethode(tekst3, datum3));
     }
 
+    public boolean PTMethode(String invoer, LocalDate date, int chars, boolean isJuist) {
+        LocalDate vandaag = LocalDate.now();
+        int comparison = date.compareTo(vandaag);
+
+        if (!(invoer.equalsIgnoreCase("Toets") || invoer.equalsIgnoreCase("Huiswerk"))) {
+            return false;
+        }
+        if (!(comparison > -1)) {
+            return false;
+        }
+        if (chars > 249) {
+            return false;
+        }
+        return isJuist;
+    }
+
+    @Order(3)
     @Test
-    void pairwiseTest(){
+    void pairwiseTest() {
+        LocalDate testDate1 = LocalDate.of(2024, 4, 4);
+        LocalDate testDate2 = LocalDate.of(1999, 1, 2);
+        LocalDate testDate3 = LocalDate.now();
+        assertFalse(PTMethode("toets", testDate2, 244, true));
+        assertFalse(PTMethode("hallo", testDate1, 123, true));
+        assertFalse(PTMethode("Toets", testDate1, 200, false));
+        assertFalse(PTMethode("Huiswerk", testDate1, 250, true));
+
+        assertTrue(PTMethode("Huiswerk", testDate1, 15, true));
+        assertTrue(PTMethode("toets", testDate3, 20, true));
 
 
     }
+
+    @Order(1)
     @Test
-    void MCDC1eTrue () throws ParseException {
+    void allMCDCTest() {
+    }
+
+    @Disabled
+    @Test
+    void MCDC1eTrue() throws ParseException {
         SDLAMain test = new SDLAMain();
         test.addDeadline();
         assertEquals(1, test.opslag.size());
     }
+
+    @Disabled
     @Test
-    void MCDC2eTrue () throws ParseException {
+    void MCDC2eTrue() throws ParseException {
         SDLAMain test = new SDLAMain();
         test.addDeadline();
         assertEquals(1, test.opslag.size());
     }
 
+    @Disabled
     @Test
-    void MCDC1eFalse () throws ParseException {
+    void MCDC1eFalse() throws ParseException {
         SDLAMain test = new SDLAMain();
         test.addDeadline();
         assertEquals(0, test.opslag.size());
     }
 
+    @Disabled
     @Test
-    void MCDC2eFalse () throws ParseException {
+    void MCDC2eFalse() throws ParseException {
         SDLAMain test = new SDLAMain();
         test.addDeadline();
         assertEquals(0, test.opslag.size());
     }
 
+    @Disabled
     @Test
-    void MCDC3eFalse () throws ParseException {
+    void MCDC3eFalse() throws ParseException {
         SDLAMain test = new SDLAMain();
         test.addDeadline();
         assertEquals(0, test.opslag.size());
     }
 
+    @Disabled
     @Test
-    void MCDC4eFalse () throws ParseException {
+    void MCDC4eFalse() throws ParseException {
         SDLAMain test = new SDLAMain();
         test.addDeadline();
-        assertEquals(0, test.opslag.size()); }
-
-        public boolean eqMethode(String x, LocalDate y){
-            LocalDate vandaag = LocalDate.now();
-            int comparison= y.compareTo(vandaag);
-        //zelfde voorwaarde als in de addDeadline methode
-       return x.length() <= 249 && comparison > -1;
+        assertEquals(0, test.opslag.size());
     }
-
-
-
-
 
 
 }

407a1e4e22d4961674511834259713af71838732
imranm20 <80264994+imranm20@users.noreply.github.com>
2021-05-09 02:16:39 +0200

    MCDC test

--- src/SDLAMainTest.java
+++ src/SDLAMainTest.java
@@ -1,8 +1,49 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import java.text.ParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class SDLAMainTest {
+class SDLAMainTest{
 
     @org.junit.jupiter.api.Test
-    void addDeadline() {
+    void MCDC1eTrue () throws ParseException {
+        SDLAMain test = new SDLAMain();
+        test.addDeadline();
+        assertEquals(1, test.opslag.size());
+    }
+    @org.junit.jupiter.api.Test
+    void MCDC2eTrue () throws ParseException {
+        SDLAMain test = new SDLAMain();
+        test.addDeadline();
+        assertEquals(1, test.opslag.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void MCDC1eFalse () throws ParseException {
+        SDLAMain test = new SDLAMain();
+        test.addDeadline();
+        assertEquals(0, test.opslag.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void MCDC2eFalse () throws ParseException {
+        SDLAMain test = new SDLAMain();
+        test.addDeadline();
+        assertEquals(0, test.opslag.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void MCDC3eFalse () throws ParseException {
+        SDLAMain test = new SDLAMain();
+        test.addDeadline();
+        assertEquals(0, test.opslag.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void MCDC4eFalse () throws ParseException {
+        SDLAMain test = new SDLAMain();
+        test.addDeadline();
+        assertEquals(0, test.opslag.size());
     }
 }

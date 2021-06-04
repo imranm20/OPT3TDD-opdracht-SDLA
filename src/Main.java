f485e7ebee8daac19004f05416216c33c31a36eb
imranm20 <80264994+imranm20@users.noreply.github.com>
2021-06-04 14:22:46 +0200

    herkansing

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    private SDLAmethod inMain1 = new SDLAmethod();
-    private AgendaData inMain2 = new AgendaData();
+    public SDLAmethod sdlaMethodObject = new SDLAmethod();
+    public AgendaData agendaDataObject = new AgendaData();
     public static void menu() {
         System.out.println("welkom bij het menu");
         System.exit(0);
     }
     public static void main(String[] args) {
-
 
     }
 }
@@ -19,58 +18,59 @@ public class Main {
 class AgendaData {
     public static ArrayList<Opdracht> opslag = new ArrayList<>();
 
-    public Opdracht getDatum() {
+    public Opdracht getOpdrachtenOpDatum() {
         Opdracht returned = null;
+
 
         return returned;
     }
 }
 
 class SDLAmethod {
-      Scanner s = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
 
-    public void addDeadline() throws ParseException {
-        System.out.println("Welkom bij het toevoegen van een deadline, als u wilt stoppen  type dan" + "'stop'");
+    public void addOpdracht() throws ParseException {
+        System.out.println("Welkom bij het toevoegen van een opdracht, als u wilt stoppen  type dan" + "'stop'");
         System.out.println("Geef de datum in dit format: jjjj-mm-dd");
 
-        String regDate = s.nextLine();
+        String regDate = scanner.nextLine();
         LocalDate vandaag = LocalDate.now();
         LocalDate date1 = LocalDate.parse(regDate);
         int comparison = date1.compareTo(vandaag);
         if (regDate.equals("stop") || (comparison > -1)) {
             Main.menu();       }
 
-        System.out.println("Geef de naam van de deadline:");
-        String naam1 = s.nextLine();
+        System.out.println("Geef de naam van de opdracht:");
+        String naam1 = scanner.nextLine();
         if (naam1.equals("stop")) {
             Main.menu();        }
         System.out.println("Geef de naam van het vak:");
-        String vaknaam1 = s.nextLine();
+        String vaknaam1 = scanner.nextLine();
         if (vaknaam1.equals("stop")) {
             Main.menu();        }
-        System.out.println("Geef de omschrijving over deze deadline:");
-        String info1 = s.nextLine();
+        System.out.println("Geef de omschrijving over deze opdracht:");
+        String info1 = scanner.nextLine();
         if (info1.equals("stop") || info1.length() <= 249) {
             Main.menu();       }
-        System.out.println("Voer de soort deadline in ( toets/huiswerk )");
-        String dd = s.nextLine();
+        System.out.println("Voer de soort opdracht in ( toets/huiswerk )");
+        String dd = scanner.nextLine();
         if (dd.equals("stop") || !(dd.equalsIgnoreCase("toets") || dd.equalsIgnoreCase("huiswerk"))) {
             Main.menu();        }
         if (dd.equalsIgnoreCase("toets")) {
-            s.nextLine();
+            scanner.nextLine();
             System.out.println("Voer het aantal studiepunten in(geheel nummer))");
-            String stdinvoer = s.nextLine();
+            String stdinvoer = scanner.nextLine();
             if (stdinvoer.equals("stop")) {
                 Main.menu();            }
             int std = Integer.parseInt(stdinvoer);
             System.out.println("Voer het aantal minuten in(geheel nummer))");
-            String mininvoer = s.nextLine();
+            String mininvoer = scanner.nextLine();
             if (mininvoer.equals("stop")) {
                 Main.menu();            }
             int duur = Integer.parseInt(mininvoer);
-            s.nextLine();
+            scanner.nextLine();
             System.out.println("Geef het lokaalnummer");
-            String lokaal = s.nextLine();
+            String lokaal = scanner.nextLine();
             if (lokaal.equals("stop")) {
                 Main.menu();
             } else {
@@ -79,12 +79,12 @@ class SDLAmethod {
             }
              else if (dd.equalsIgnoreCase("huiswerk")) {
             System.out.println("Voer je teamgenoten in");
-            String team1 = s.nextLine();
+            String team1 = scanner.nextLine();
             if (team1.equals("stop")) {
                 Main.menu();
             }
             System.out.println("Geef de prioriteit op een schaal van tien(geheel nummer)");
-            String prio = s.nextLine();
+            String prio = scanner.nextLine();
             int prioriteit = Integer.parseInt(prio);
             if (prio.equals("stop")) {
                 Main.menu();
@@ -95,8 +95,6 @@ class SDLAmethod {
         }
     }
 
-    public void addInfo(){
-    }
 }
 
 abstract class Opdracht {
